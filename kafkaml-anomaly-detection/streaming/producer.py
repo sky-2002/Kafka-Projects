3778fb56d1b93c2c63ddfcf82d45e7a4d90adb82
Aakash Thatte <aathatte2002@gmail.com>
2022-06-10 19:20:50 +0530

    Add code to include anomaly detection example

---
+++ kafkaml-anomaly-detection/streaming/producer.py
@@ -0,0 +1,46 @@
+import json
+import random
+import time
+from datetime import datetime
+
+import numpy as np
+
+# from settings import TRANSACTIONS_TOPIC, DELAY, OUTLIERS_GENERATION_PROBABILITY
+# from streaming.utils import create_producer
+from utils import create_producer
+
+#-------from settings was giving import errors, so I am adding this
+DELAY = 0.01
+NUM_PARTITIONS = 3
+OUTLIERS_GENERATION_PROBABILITY = 0.2
+KAFKA_BROKER = "localhost:9092"
+TRANSACTIONS_TOPIC = "transactions"
+TRANSACTIONS_CONSUMER_GROUP = "transactions"
+ANOMALIES_TOPIC = "anomalies"
+ANOMALIES_CONSUMER_GROUP = "anomalies"
+#-------------------------------------------------------------------
+
+_id = 0
+producer = create_producer()
+
+if producer is not None:
+    while True:
+        # Generate some abnormal observations
+        if random.random() <= OUTLIERS_GENERATION_PROBABILITY:
+            X_test = np.random.uniform(low=-4, high=4, size=(1, 2))
+        else:
+            X = 0.3 * np.random.randn(1, 2)
+            X_test = (X + np.random.choice(a=[2, -2], size=1, p=[0.5, 0.5]))
+
+        X_test = np.round(X_test, 3).tolist()
+
+        current_time = datetime.utcnow().isoformat()
+
+        record = {"id": _id, "data": X_test, "current_time": current_time}
+        record = json.dumps(record).encode("utf-8")
+
+        producer.produce(topic=TRANSACTIONS_TOPIC,
+                         value=record)
+        producer.flush()
+        _id += 1
+        time.sleep(DELAY)
